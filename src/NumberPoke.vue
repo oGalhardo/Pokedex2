2a003e15b3c396173871a2acb132a381c3171b7e
Junior <juniorwgalhardo@aluno.santoangelo.uri.br>
2023-08-10 14:24:47 -0300

    V1 Pokedex OFC

---
+++ src/NumberPoke.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <input type="number" name="nameNum" id="nameNumId">
+        <button @click="enviaValor">Pesquisar Pokemon</button>
+    </div>
+</template>
+<script>
+export default{
+    methods:{
+        enviaValor(){
+            const valor = (document.getElementById('nameNumId'))
+            const num = parseInt(valor.value)
+            this.$emit('valor-recebido', num)
+        }
+    }
+    
+}
+</script>
