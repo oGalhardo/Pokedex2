fb8322df7860a5c5a4ed6da2ce660c0bf1aec2d3
Junior <juniorwgalhardo@aluno.santoangelo.uri.br>
2023-08-22 09:37:53 -0300

    Pokemons por geracao e Pesquisa Prox e Ant

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,54 @@
-
-
 <template>
-  <header>
-    <home-pokedex/>
-  </header>
+  <div>
+    <h1>Pokedex</h1>
+    <br />
+    <h6>Digite o Nome do Pokemon e pressione Enter</h6>
+    <input type="text" v-model="nome" @keyup.enter="searchPokemon(nome)" />
+    <!-- <div v-if="sprite != ''">
+      <img :src="sprite" alt="Pokemon non existe" />
+      <button @click="attSprite(idPokemo - 1)">Back</button>
+      <br />
+      <button @click="attSprite(idPokemo + 1)">Next</button>
+    </div> -->
+    <div>
+      Selecione a geração de Pokemons
+      <button @click="nextGeneration('generation-i')">Next Generation</button>
+      <button @click="backGeneration">Back Generation</button>
+    </div>
+    <div>
+      <img v-for="pokeImg in allGenPokemon" :key="pokeImg" :src="pokeImg" alt="Pokemon Sprite" />
+    </div>
+  </div>
 </template>
 <script>
-import HomePokedex from './view/HomePokedex.vue';
+import { getImage, getPokemon, getIdPokemon, getSpecies, getAllPokemonsOfGeneration } from './api'
 export default {
   name: 'App',
-  components: {
-    HomePokedex
-    
+  data() {
+    return {
+      nome: '',
+      idPokemo: '',
+      objPokemon: '',
+      espPokemon: '',
+      genPokemon: '',
+      allGenPokemon: ['']
+    }
+  },
+  methods: {
+    async searchPokemon(nomePokemon) {
+      this.objPokemon = await getPokemon(nomePokemon)
+      this.idPokemo = await getIdPokemon(nomePokemon)
+      this.sprite = await getImage(this.idPokemo)
+      this.espPokemon = await getSpecies(this.idPokemo)
+    },
+    attSprite(id) {
+      this.searchPokemon(id)
+    },
+    async nextGeneration(gen) {
+      this.allGenPokemon = await getAllPokemonsOfGeneration(gen)
+      console.log(this.genPokemon)
+    }
   }
-};
+}
 </script>
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+.section
